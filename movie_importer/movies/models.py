4f1d7c494dc37390ff35ad33de967889a7a36c03
Szymon Miks <szymon.miks@dlabs.pl>
2019-05-13 17:45:25 +0200

    Created models Name and Title
    Created models with will be hold imported data

--- movie_importer/movies/models.py
+++ movie_importer/movies/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Name(models.Model):
+    imdb_name_id = models.CharField(max_length=20, unique=True)
+    primary_name = models.CharField(max_length=120)
+    birth_year = models.IntegerField(blank=True)
+    death_year = models.IntegerField(blank=True)
+    primary_profession = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.primary_name
+
+    class Meta:
+        ordering = ('imdb_name_id',)
+
+
+class Title(models.Model):
+    imdb_title_id = models.CharField(max_length=20, unique=True)
+    type = models.CharField(max_length=120)
+    primary_title = models.CharField(max_length=200)
+    original_title = models.CharField(max_length=200)
+    is_adult = models.BooleanField()
+    start_year = models.IntegerField()
+    end_year = models.IntegerField(blank=True)
+    runtime_minutes = models.IntegerField(blank=True)
+    genres = models.CharField(max_length=250)
+    author = models.ForeignKey(Name, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.original_title
+
+    class Meta:
+        ordering = ('imdb_title_id',)
